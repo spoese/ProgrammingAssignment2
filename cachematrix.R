4245f112c63c7391a9d7aba14a4c21c073ce17dc
spoese <scott.poese@gmail.com>
2018-05-25 09:24:07 -0400

    Testing GitHub
    Making sure I know how to commit any changes correctly.

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## This is just testing GitHub interaction.
 
 ## Write a short comment describing this function
 
